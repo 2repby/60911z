6bc698f47a838abaebbf67c7459412b73bdd6c98
User <2repby@gmail.com>
2025-10-20 12:21:20 +0500

    DB connect

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+    require "dbconnect.php";
     echo "609-11z Hello2025";
-    phpinfo();
+    $result = $conn->query("SELECT * FROM CATEGORY");
+    echo '<table border="1">';
+    while ($row = $result->fetch()) {
+        echo '<tr>';
+        echo '<td>' . $row['id'] . '</td><td>' . $row['name'] . '</td>';
+        echo '<td>' . $row['id_user'] . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+
 
 
